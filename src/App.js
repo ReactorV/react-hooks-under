957cda0ab75fac7d13b81d11284165c6f236968b
Vadim Voytkus <vadim.voytkus@amtote.com>
2023-01-07 21:00:39 +0100

    add test debounce

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import './App.css';
 
 function App() {
+    function fetchUrl(url) {
+        console.log(`fetching ${url}...`, this.firstName);
+    }
+
+    const user = {
+        firstName: 'Bob'
+    }
+
+    function debounce(callback, delay) {
+        let timer;
+
+            return (url) => {
+                if (timer) {
+                    clearTimeout(timer);
+                }
+
+                timer = setTimeout(() => {
+                    callback(url);
+                }, delay)
+            }
+    }
+
+    const fetching = debounce(fetchUrl.bind(user), 300);
+
+    fetching(1);
+    fetching(2);
+    fetching(3);
+    fetching(4);
+    fetching(5);
+
   return (
     <div className="App">
       React hooks tutorial
